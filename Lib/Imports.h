ac8bccb28b7a452c974e8012dbf07173b2be0407
Terrance <terrance.hanlon@ndsu.edu>
2020-01-09 17:32:18 -0600

    Import master header file

---
+++ Lib/Imports.h
@@ -0,0 +1,17 @@
+#ifndef IMPORTS_H
+#define IMPORTS_H
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+
+#include "AnimatedSprite.hpp"
+#include "Animation.hpp"
+
+class Imports {
+
+};
+
+#endif
