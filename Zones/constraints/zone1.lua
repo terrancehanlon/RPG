081eca623960f6d0d2cb248ce1ae6d578fcb794f
Terrance <terrance.hanlon@ndsu.edu>
2020-01-16 18:28:02 -0600

    notes and lua

---
+++ Zones/constraints/zone1.lua
@@ -0,0 +1,56 @@
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
+water = {
+    x = 324,
+    y = 150
+}
+bridge = { 
+    x = 150,
+    y = 150
+}
